54b63cac67b76735398f2fd2b91c6946d778897a
Ante Kegalj <akegalj@gmail.com>
2016-07-19 00:27:46 +0200

    Extend example with effects

--- src/Main.purs
+++ src/Main.purs
@@ -6,32 +6,29 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Signal.Channel (CHANNEL)
 
-import Pux (renderToDOM, fromSimple, start)
+import Pux (renderToDOM, start, EffModel, noEffects)
 import Pux.Html (Html, text, button, span, div)
 import Pux.Html.Events (onClick)
 
-data Action = Increment | Decrement
+data Action = Button
 
 type State = Int
 
-update :: Action -> State -> State
-update Increment count = count + 1
-update Decrement count = count - 1
+update :: forall eff. Action -> State -> EffModel State Action (eff)
+update Button state = noEffects state
 
 view :: State -> Html Action
 view count =
   div
     []
-    [ button [ onClick (const Increment) ] [ text "Increment" ]
-    , span [] [ text (show count) ]
-    , button [ onClick (const Decrement) ] [ text "Decrement" ]
+    [ button [ onClick (const Button) ] [ text "Button" ]
     ]
 
 main :: forall e. Eff (err :: EXCEPTION, channel :: CHANNEL | e) Unit
 main = do
   app <- start
     { initialState: 0
-    , update: fromSimple update
+    , update: update
     , view: view
     , inputs: []
     }
